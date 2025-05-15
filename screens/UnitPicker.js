22cfedbb8fc5dfe907d79b6f51fc6a4e829aa94d
JieunK <kje147459@gmail.com>
2025-05-15 16:58:15 +0900

    modify and add units

--- screens/UnitPicker.js
+++ screens/UnitPicker.js
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DropDownPicker from "react-native-dropdown-picker";
 import { View } from "react-native";
 
-const UnitPicker = ({ onSelect }) => {
+const UnitPicker = ({ onSelect, selected }) => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([
     { label: "개", value: "EA" },
     { label: "병", value: "병" },
-    { label: "KG", value: "KG" },
-    { label: "G", value: "G"},
-    { label: "봉", value: "봉"},
-    { label: "ml", value: "ml"},
-    { label: "L", value: "L"},
-  ]);  
+    { label: "kg", value: "kg" },
+    { label: "g", value: "g" },
+    { label: "봉", value: "봉" },
+    { label: "ml", value: "ml" },
+    { label: "L", value: "L" },
+    { label: "없음", value: "없음" },
+  ]);
+
+  // selected prop이 주어지면 초기값 설정
+  useEffect(() => {
+    if (selected) {
+      setValue(selected);
+    }
+  }, [selected]);
 
   return (
     <View>
@@ -22,9 +30,10 @@ const UnitPicker = ({ onSelect }) => {
         value={value}
         items={items}
         setOpen={setOpen}
-        setValue={(val) => {
-          setValue(val);
-          onSelect(val);
+        setValue={(callback) => {
+          const newValue = callback(value); // callback(value)로 새 값 얻음
+          setValue(newValue);
+          onSelect(newValue);
         }}
         setItems={setItems}
         placeholderStyle={{
@@ -33,15 +42,15 @@ const UnitPicker = ({ onSelect }) => {
         }}
         placeholder="단위를 선택하세요"
         style={{
-            height: 40,
-            width: "100%",
-            borderColor: "#ccc",
-            borderRadius: 10,
-          }}
-          containerStyle={{
-            marginBottom: 10,
-            borderColor: "#000",
-          }}
+          height: 40,
+          width: "100%",
+          borderColor: "#ccc",
+          borderRadius: 10,
+        }}
+        containerStyle={{
+          marginBottom: 10,
+          borderColor: "#000",
+        }}
         zIndex={2000}
         elevation={2}
       />
